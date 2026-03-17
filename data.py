335fe3346fa811e8a3c81fc49339fa612bb2c199
GorshkovRoman <Rizaror@yandex.ru>
2026-03-17 15:58:14 +0300

    Вторая часть проекта

---
+++ data.py
@@ -0,0 +1,11 @@
+ORDER_BODY = {
+    "firstName": "Антонио",
+    "lastName": "Бандерос",
+    "address": "Улица Яндекс, дом 44",
+    "metroStation": 4,
+    "phone": "+79123456789",
+    "rentTime": 5,
+    "deliveryDate": "2026-03-16",
+    "comment": "Автотест",
+    "color": ["BLACK"]
+}
